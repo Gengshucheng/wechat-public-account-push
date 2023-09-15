93a17800d2c239be738d7bd94ee260dd6a0140f0
gsc1998 <74408885+Gengshucheng@users.noreply.github.com>
2023-09-15 16:16:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,10 +8,10 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx65e539f66617f87c',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '38503373db175e0ddef587e8b179dafa',
 
   PROVINCE: '广东',
   CITY: '惠州',
@@ -19,11 +19,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '马小涵',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o-3Ja6wfG-r4LzaoVnXL5tSs4zPQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'G6gjkS6hVIwfoDPub2PRtXWZWNz1KULcSeqSZIj5qdE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -42,14 +42,17 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-03-07' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
 
-
+  TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: '36b481cda282fb9b7e00eb06e4c7b5d4',
+  },
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '',
 
